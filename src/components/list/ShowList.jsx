154e4799295cc7bdec23d83501fff575fee35920
KristinaBatyo <105168024+KristinaBatyo@users.noreply.github.com>
2023-08-27 15:18:24 +0300

    fix

--- src/components/list/ShowList.jsx
+++ src/components/list/ShowList.jsx
@@ -14,7 +14,7 @@ import {
   FullScreenAlert,
   FullScreenText,
 } from "./ShowList.styled";
-import { Alert, Container, Paper, Rating, } from "@mui/material";
+import { Alert, Paper, Rating } from "@mui/material";
 
 function ShowList() {
   const [searchText, setSearchText] = useState("");
@@ -37,72 +37,66 @@ function ShowList() {
   }, [searchText]);
 
   return (
-    <Container>
-      <SearchContainer>
-        <StyledTextField
-          id="outlined-basic"
-          label="Search for a show..."
-          value={searchText}
-          onChange={(e) => setSearchText(e.target.value)}
-        />
-        {searchText.length < 2 ? (
-          <FullScreenText>
-            <FullScreenAlert icon={false} severity="success">
-              Type the show's name
-            </FullScreenAlert>
-          </FullScreenText>
-        ) : (
-          <ShowListContainer>
-            {searchResults.length === 0 ? (
-              <Alert severity="info">
-                Sorry, nothing found with this search
-              </Alert>
-            ) : (
-              <List>
-                {searchResults.map((result) => (
-                  <ShowItem key={result.show.id}>
-                    <Paper
-                      sx={{
-                        p: 2,
-                        margin: "auto",
-                        maxWidth: 500,
-                        flexGrow: 1,
-                        backgroundColor: (theme) =>
-                          theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-                      }}
-                    >
-                      <ShowLink to={`/details/${result.show.id}`}>
-                        <ShowImage
-                          src={
-                            result.show.image ? result.show.image.medium : ""
-                          }
-                          alt={result.show.name}
-                        />
-                        <div>
-                          <ShowTitle>{result.show.name}</ShowTitle>
-                          <RatingContainer>
-                            <Rating
-                              name="read-only"
-                              value={result.show.rating.average}
-                              max={10}
-                              precision={0.5}
-                              readOnly
-                            />
-                            <ShowRating>
-                              {result.show.rating.average}
-                            </ShowRating>
-                          </RatingContainer>
-                        </div>
-                      </ShowLink>
-                    </Paper>
-                  </ShowItem>
-                ))}
-              </List>
-            )}
-          </ShowListContainer>
-        )}
-      </SearchContainer>
-    </Container>
+    <SearchContainer>
+      <StyledTextField
+        id="outlined-basic"
+        label="Search for a show..."
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+      />
+      {searchText.length < 2 ? (
+        <FullScreenText>
+          <FullScreenAlert icon={false} severity="success">
+            Type the show's name
+          </FullScreenAlert>
+        </FullScreenText>
+      ) : (
+        <ShowListContainer>
+          {searchResults.length === 0 ? (
+            <Alert severity="info">Sorry, nothing found with this search</Alert>
+          ) : (
+            <List>
+              {searchResults.map((result) => (
+                <ShowItem key={result.show.id}>
+                  <Paper
+                    sx={{
+                      p: 2,
+                      margin: "auto",
+                      maxWidth: 500,
+                      flexGrow: 1,
+                      backgroundColor: (theme) =>
+                        theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+                    }}
+                  >
+                    <ShowLink to={`/details/${result.show.id}`}>
+                      <ShowImage
+                        src={result.show.image ? result.show.image.medium : ""}
+                        alt={result.show.name}
+                      />
+                      <div>
+                        <ShowTitle>{result.show.name}</ShowTitle>
+                      </div>
+                    </ShowLink>
+                    <RatingContainer>
+                      <Rating
+                        name="size-small"
+                        defaultValue={2}
+                        size="small"
+                        value={result.show.rating.average}
+                        max={10}
+                        precision={0.5}
+                        readOnly
+                      />
+                      <ShowRating>{result.show.rating.average}</ShowRating>
+                    </RatingContainer>
+                  </Paper>
+                </ShowItem>
+              ))}
+            </List>
+          )}
+        </ShowListContainer>
+      )}
+    </SearchContainer>
   );
 }
 
--- src/components/list/ShowList.styled.jsx
+++ src/components/list/ShowList.styled.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { TextField } from "@mui/material";
 import { Alert } from "@mui/material";
 
+
 export const FullScreenAlert = styled(Alert)`
   min-width: 200px;
   background-color: white;
@@ -11,17 +12,18 @@ export const FullScreenAlert = styled(Alert)`
   align-items: center;
 `;
 export const FullScreenText = styled.div`
-  min-width: 100vw;
-  height: 100vh;
+  max-width: 100%;
+  max-height: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
-
 `;
 
 export const StyledTextField = styled(TextField)`
-  width: 100%;
+  max-width: 100vq;
   margin-bottom: 10px;
+  padding: 0;
+  min-width: 80vw;
 
   & .MuiOutlinedInput-root {
     background-color: rgb(237, 247, 237);
@@ -48,28 +50,6 @@ export const SearchContainer = styled.div`
   align-items: center;
   justify-content: center;
   flex-direction: column;
-  padding: 20px;
-`;
-
-export const SearchInput = styled.input`
-  width: 80%;
-  padding: 10px;
-  font-size: 16px;
-  background-color: #18333e;
-  color: white;
-  border-radius: 25px;
-  margin-bottom: 20px;
-  border: 2px solid transparent; 
-  transition: border-color 0.3s; 
-
-  &:focus {
-    border-color: #18333e; 
-  }
-`;
-
-export const Message = styled.p`
-  font-size: 16px;
-  text-align: center;
 `;
 
 export const ShowListContainer = styled.div`
@@ -79,19 +59,22 @@ export const ShowListContainer = styled.div`
 export const RatingContainer = styled.div`
   display: flex;
   align-items: center;
-  justify-content: center;
+  margin-top: 20px;
 `;
 export const List = styled.ul`
   list-style: none;
-  height: 100%;
   background-color: rgb(237, 247, 237);
   margin: 0px;
   padding: 15px;
 `;
 
 export const ShowItem = styled.li`
+  white-space: nowrap;
   &:not(:last-child) {
     margin-bottom: 20px;
+  }
+  @media (max-width: 768px) {
+    max-width: 100%; 
   }
 `;
 
@@ -112,10 +95,15 @@ export const ShowImage = styled.img`
 export const ShowTitle = styled.h3`
   font-size: 18px;
   margin: 0;
+  @media (max-width: 768px) {
+    font-size: 12px;
+  }
 `;
 
 export const ShowRating = styled.p`
   font-size: 16px;
-  margin-left: 20px;
+  margin-left: 10px;
   color: gold;
+  @media (max-width: 768px) {
+    font-size: 14px; 
 `;
